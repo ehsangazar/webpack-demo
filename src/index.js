e6cd2a5bdb65e8dcb476c165f6365cd7fd82031e
Ehsan Gazar <me@ehsangazar.com>
2020-08-11 12:57:35 +1000

    webpack dev

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const SampleComponent = React.createElement('div',{},'This is Sample React Component')
+const SampleComponent = React.createElement('div',{},'This is Sample React Component Version 7')
 
 ReactDOM.render(SampleComponent,document.getElementById('App'));
